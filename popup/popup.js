667029c8dece21cd372b1ebe4c9aadbba65bdefa
Abhi.N <abhinew1120@gmail.com>
2024-06-17 20:54:17 -0400

    added button to send message to contentscript

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,17 @@
-console.log('working');
+let button = document.getElementById("button");
+
+button.addEventListener("click", sendMessageToContentscript);
+
+function sendMessageToContentscript() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        message: "Form Filler Button Has Been Clicked",
+      },
+      (response) => {
+        console.log(response.response);
+      }
+    );
+  });
+}
