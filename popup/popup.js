e293be8a401c6fcad12e4ab87ffc90522a873bbf
Abhi.N <abhinew1120@gmail.com>
2024-06-17 21:43:52 -0400

    sent message to contentscript

--- popup/popup.js
+++ popup/popup.js
@@ -3,9 +3,13 @@ let button = document.getElementById("button");
 button.addEventListener("click", sendMessageToContentscript);
 
 function sendMessageToContentscript() {
+  console.log("button clicked");
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      command: "fill",
-    });
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        command: "fill",
+      }
+    );
   });
 }
