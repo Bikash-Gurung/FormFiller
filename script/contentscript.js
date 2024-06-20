6fa97cc9447364c9f09d3f6a3f8dbbc01575441f
oasisneupane <oasisneupane@gmail.com>
2024-06-19 21:58:20 -0400

    made proper changes in the contentscript file

--- script/contentscript.js
+++ script/contentscript.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.command == "fill"){
-        console.log("Message recived.");
+    if(request.command.toLowerCase() == "fill"){
+        console.log("message recived.");
     }
 });
